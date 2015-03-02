696bdcbcdf591820f084fe318f0662d4286e27f2
Pragya Daga <dagapragya@hotmail.com>
2015-03-02 06:55:09 +0530

    Added the for loop checking condition

--- finding_function_calls.cpp
+++ finding_function_calls.cpp
@@ -1,7 +1,7 @@
 #include "finding_function_calls.h"
 bool matchFunctionCall(const std::string& input)
 {
-	const char *for_const ="for";
+	const char *for_const ="for(";
 	if(input.rfind(for_const,0)!=0)
 	{
 		std::regex regex_call("[[:alpha:]][[:alpha:][:digit:]_]*\\(([[:alpha:]][[:alpha:][:digit:]_]*:[^:]*)(,[[:alpha:]][[:alpha:][:digit:]_]*:[^:]*)*\\)");
