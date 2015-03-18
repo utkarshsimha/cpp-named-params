bffbd20ff93f0be474e076a66adf21df326bbf01
Utkarsh Simha <utkarshsimha@gmail.com>
2015-03-18 23:46:43 +0530

    Extracting function name and params

--- finding_def.cpp
+++ finding_def.cpp
@@ -2,9 +2,13 @@
 #include "finding_def.hpp"
 bool matchFunctionDef(const std::string& input)
 {
-	std::regex regex_call("(virtual|friend)?[ ]?((bool|void|double|int))[ ]([[:alpha:]][[:alpha:][:digit:]_]*)\\(((.*)[ ]*([[:alpha:]][[:alpha:]]*)[,])*(.*)[ ]*([[:alpha:]][[:alpha:]]*)*\\)");
-	if(std::regex_match(input,regex_call))
-		return true;
-	else
-		return false;
+    std::smatch func_def_match;
+    std::regex regex_def("(virtual|friend)?[ ]?(bool|void|double|int)[ ]([[:alpha:]][[:alpha:][:digit:]_]*)\\((((.*)[ ]*([[:alpha:]][[:alpha:]]*)[,])*(.*)[ ]*([[:alpha:]][[:alpha:]]*)*)\\)");
+    if(std::regex_match(input,func_def_match,regex_def)) {
+        std::string func_name(func_def_match.str(3)); //Extracting function name
+        std::string func_param(func_def_match.str(4)); //Extracting function params
+        return true;
+    }
+    else
+        return false;
 }
